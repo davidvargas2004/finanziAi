6e4f44e99c1450800143cb5ea50a85df5583fe2b
juan balaguera <146900241+j21bala@users.noreply.github.com>
2025-04-02 03:44:56 +0000

    InicioSesionFrm

--- finanziai/src/App.jsx
+++ finanziai/src/App.jsx
@@ -8,103 +8,58 @@ function App() {
 
   return (
     <>
-          
-    <form class="modern-form">
-      <div class="form-title">Registro</div>
-
-      <div class="form-body">
-        <div class="input-group">
-          <div class="input-wrapper">
-            <svg fill="none" viewBox="0 0 24 24" class="input-icon">
-              <circle
-                stroke-width="1.5"
-                stroke="currentColor"
-                r="4"
-                cy="8"
-                cx="12"
-              ></circle>
+      
+    <div class="card">
+      <div class="card2">
+        <form class="form">
+          <p id="heading">Iniciar sesión</p>
+          <div class="field">
+            <svg
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              height="16"
+              width="16"
+              xmlns="http://www.w3.org/2000/svg"
+              class="input-icon"
+            >
               <path
-                stroke-linecap="round"
-                stroke-width="1.5"
-                stroke="currentColor"
-                d="M5 20C5 17.2386 8.13401 15 12 15C15.866 15 19 17.2386 19 20"
+                d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"
               ></path>
             </svg>
             <input
-              required=""
-              placeholder="Usuario"
-              class="form-input"
               type="text"
+              class="input-field"
+              placeholder="Usuario"
+              autocomplete="off"
             />
           </div>
-        </div>
-
-        <div class="input-group">
-          <div class="input-wrapper">
-            <svg fill="none" viewBox="0 0 24 24" class="input-icon">
+          <div class="field">
+            <svg
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              height="16"
+              width="16"
+              xmlns="http://www.w3.org/2000/svg"
+              class="input-icon"
+            >
               <path
-                stroke-width="1.5"
-                stroke="currentColor"
-                d="M3 8L10.8906 13.2604C11.5624 13.7083 12.4376 13.7083 13.1094 13.2604L21 8M5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19Z"
+                d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"
               ></path>
             </svg>
-            <input
-              required=""
-              placeholder="Correo Electrónico"
-              class="form-input"
-              type="email"
-            />
+            <input type="password" class="input-field" placeholder="Contraseña" />
           </div>
-        </div>
-
-        <div class="input-group">
-          <div class="input-wrapper">
-            <svg fill="none" viewBox="0 0 24 24" class="input-icon">
-              <path
-                stroke-width="1.5"
-                stroke="currentColor"
-                d="M12 10V14M8 6H16C17.1046 6 18 6.89543 18 8V16C18 17.1046 17.1046 18 16 18H8C6.89543 18 6 17.1046 6 16V8C6 6.89543 6.89543 6 8 6Z"
-              ></path>
-            </svg>
-            <input
-              required=""
-              placeholder="Contraseña"
-              class="form-input"
-              type="password"
-            />
-            <button class="password-toggle" type="button">
-              <svg fill="none" viewBox="0 0 24 24" class="eye-icon">
-                <path
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  d="M2 12C2 12 5 5 12 5C19 5 22 12 22 12C22 12 19 19 12 19C5 19 2 12 2 12Z"
-                ></path>
-                <circle
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  r="3"
-                  cy="12"
-                  cx="12"
-                ></circle>
-              </svg>
+          <div class="btn">
+            <button class="button1">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Iniciar Sesión&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             </button>
+            <button class="button2">Registrarse</button>
           </div>
-        </div>
+          <button class="button3">Recuperar contraseña</button>
+        </form>
       </div>
-    
-      <button class="submit-button" type="submit">
-        <span class="button-text">Crear cuenta</span>
-        <div class="button-glow"></div>
-      </button>
+    </div>
 
-      <div class="form-footer">
-        <a class="login-link" href="#">
-          ¿Ya tienes una cuenta? <span>Inicia Sesión</span>
-        </a>
-      </div>
-    </form>
-
-        
+            
     </>
   )
 }
