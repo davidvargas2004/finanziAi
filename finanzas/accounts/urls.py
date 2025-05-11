596e43450e7c543ffd82bae2b1c73863268c00a6
Cristian <trialwinner@gmail.com>
2025-05-11 18:28:25 +0000

    nombre

--- finanzas/accounts/urls.py
+++ finanzas/accounts/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('notifications/', views.notifications_view, name='notifications'),
     path('notifications/read/<int:notif_id>/', views.mark_notification_read, name='mark_notification_read'),
     path('comparativa-mensual/', views.comparativa_mensual_view, name='comparativa_mensual'),
-
+    path('grafico/', views.grafico_view, name='grafico'),
 ]
