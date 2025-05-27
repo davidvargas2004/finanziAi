f427a8bf2c4967e63746dc9956df2f1ec05b743f
Juan Balaguera <146900241+j21bala@users.noreply.github.com>
2025-05-27 15:39:37 +0000

    actualizacion_urlsJWT

--- finanzas/finanzia/urls.py
+++ finanzas/finanzia/urls.py
@@ -1,17 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings # Necesario para DEBUG y static
-from django.conf.urls.static import static # Necesario para static
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import RedirectView
+
+# Importaciones para las vistas de Simple JWT
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')), # Incluye las URLs de tu app 'accounts'
     path('', RedirectView.as_view(url='/accounts/login/', permanent=False)), # Redirige la raíz a la página de login de accounts
+
+    # ---- NUEVAS RUTAS PARA JWT ----
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # ---- FIN DE NUEVAS RUTAS ----
 ]
 
 # Servir archivos estáticos durante el desarrollo (DEBUG=True)
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    # Si también usas archivos subidos por usuarios (MEDIA_FILES):
-    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
